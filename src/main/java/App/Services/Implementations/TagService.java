d60e459a3eea5f946bc1e4ee384a12cb747f3a46
mmesihovic <mmesihovic1@etf.unsa.ba>
2018-11-06 20:13:10 +0100

    Implemented TagService and added comments for documentation

--- src/main/java/App/Services/Implementations/TagService.java
+++ src/main/java/App/Services/Implementations/TagService.java
@@ -1,8 +1,48 @@
 package App.Services.Implementations;
 
+import App.Entities.Tags;
+import App.Repositories.ITagRepository;
 import App.Services.Interfaces.ITagService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+/**
+ * CategoryService encapsulates the state information needed for get and find Tags.
+ * Uses ITagRepository.
+ * Implements ITagService
+ * @see ITagRepository
+ * @see ITagService
+ *
+ * @author  Mirza
+ * @since   1.0
+ */
 
 @Service
 public class TagService implements ITagService {
+    @Autowired
+    private ITagRepository tagRepository;
+
+    /**
+     * Returns list of all tags from Tags table in Oracle database BP20
+     *
+     * @return      list of all tags from Oracle database BP20
+     */
+
+    @Override
+    public List<Tags> getAllTags() {
+        return (List<Tags>) tagRepository.findAll();
+    }
+
+    /**
+     * Returns one tag with specified name
+     *
+     * @param name      used for finding tag with specific name
+     * @return          only one Tag find by name
+     */
+
+    @Override
+    public Tags findByName(String name) { return tagRepository.findByName(name); }
 }
--- src/main/java/App/Services/Interfaces/ITagService.java
+++ src/main/java/App/Services/Interfaces/ITagService.java
@@ -1,4 +1,18 @@
 package App.Services.Interfaces;
 
+import App.Entities.Tags;
+
+import java.util.List;
+
+/**
+ * Interface defining methods for manipulating Tags table in Oracle database.
+ *
+ * @author  Mirza
+ * @since   1.0
+ */
+
+
 public interface ITagService {
+    List<Tags> getAllTags();
+    Tags findByName(String name);
 }
